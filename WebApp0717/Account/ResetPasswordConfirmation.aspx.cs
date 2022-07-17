7c6d1002764241e3d8166709ed888f43b99c645a
xub653 <31357839+xub653@users.noreply.github.com>
2022-07-16 21:24:49 -0500

    Add project files.

---
+++ WebApp0717/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace WebApp0717.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ WebApp0717/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApp0717.Startup))]
+namespace WebApp0717
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
